915463b182cdbba05305bab59100b6d8730155d5
Connell Boyce <connell.p.boyce@gmail.com>
2020-06-05 16:31:48 -0400

    Added a file to scrape a website for pepper info
    - Runs as a thread from main
    - Saves data in local Lists and Sets

--- src/main/java/com/connellboyce/scraper/ScraperApplication.java
+++ src/main/java/com/connellboyce/scraper/ScraperApplication.java
@@ -1,5 +1,6 @@
 package com.connellboyce.scraper;
 
+import com.connellboyce.scraper.scrapers.PepperScraper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,13 @@ public class ScraperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScraperApplication.class, args);
+		startScrapers();
 	}
 
+	/**
+	 * Starts threads to run the PepperScraper file
+	 */
+	public static void startScrapers() {
+		new Thread(new PepperScraper()).start();
+	}
 }
