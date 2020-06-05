303c5ebc4faeffa93217a1ea7efe42031cfb50ef
Connell Boyce <connell.p.boyce@gmail.com>
2020-06-05 17:31:10 -0400

    Added converter to external files in json

--- src/main/java/com/connellboyce/scraper/converters/PeppersToJSONConverter.java
+++ src/main/java/com/connellboyce/scraper/converters/PeppersToJSONConverter.java
@@ -2,8 +2,12 @@ package com.connellboyce.scraper.converters;
 
 import com.connellboyce.scraper.scrapers.PepperScraper;
 import org.codehaus.jackson.JsonEncoding;
+import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.Set;
 
@@ -20,6 +24,15 @@ public class PeppersToJSONConverter {
             jsonGenerator.close();
         } catch (IOException ioException){
             System.out.println(ioException.getMessage());
+        }
+    }
+
+    public void convertToFile(Set<PepperScraper.PepperDetail> pepperSet) {
+        try {
+            //Convert object to JSON string and save into file directly
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("pepperList.json"), pepperSet);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
